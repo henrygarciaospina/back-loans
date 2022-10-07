4f85ed57097de25511ac05df632564166446f45b
henrygosling <henrygarciaospina@hotmail.com>
2022-10-06 21:19:24 -0500

    Change version Spring boot and delete dependencies

--- src/main/java/com/loans/apploans/AppLoansApplication.java
+++ src/main/java/com/loans/apploans/AppLoansApplication.java
@@ -3,11 +3,10 @@ package com.loans.apploans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
-@SpringBootApplication
-@EnableReactiveMongoAuditing
 @Configuration
+@SpringBootApplication
+
 public class AppLoansApplication {
 
 	public static void main(String[] args) {
